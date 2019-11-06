711b56aab32fab958abc2a29bdc24f724acf359f
rubeneitor <rvb1492@hotmail.com>
2019-11-06 18:04:36 +0100

    cargaCancionesYReproductor

--- js/app.js
+++ js/app.js
@@ -27,11 +27,11 @@
       for (var i = 0; i < numero; i++) {
         if (tracks[i].artwork_url !== null) {
           $('.lista').append(
-            "<div " + "id =' "+tracks[i].artwork_url +" ' class='imagen_mini col-xs-2'> " + tracks[i].title + "<br><img src='" + 
+            "<div " + "id =' "+tracks[i].artwork_url +" ' class='imagen_mini col-xs-2'><br><img src='" + 
               tracks[i].artwork_url + 
               "' id ='" + 
               tracks[i].id + console.log(tracks[i].id) + 
-              "' draggable='true' ondragstart='drag(event)'></div>"
+              `' draggable='true' ondragstart='drag(event, ${tracks[i].id})'><p class="titulo">Titulo: ` + tracks[i].title + `<p class="genero">Genero: ` + tracks[i].genre +`</p><p class="autor">Autor: ` + tracks[i].user.username + `</p></div>`
           );
         }
       }
@@ -42,42 +42,109 @@
     
    }
 
- 
-   
-
-  //  $( function() {
-  //   $( "#draggable" ).draggable();
-  // } );
-   
-  //  function reproducir(){
-  //   let arrastrar = event.target.id;
-  //   // arrastrar.event.drag;
-  //        console.log('ARRASTRAR' + arrastrar);
-  //    }
-
-  //   node.on('click', fuction ev){
-      
-  //   }
-  //     // 
-
   function permitirDrop(event){
     event.preventDefault();
    }
 
-   function drag(event){
-     event.dataTransfer.setData('data', event.target.id);
-     console.log(event.dataTransfer.getData('data', event.target.id));
+   function drag(event, pene){
+     event.dataTransfer.setData('data', pene);
+     console.log(pene);
    }
   
    function drop(event){
      event.preventDefault();
+    //  cancion3(event.target.id);
+    // asignamosCancion(event);
+    //  event.preventDefault();
     var data = event.dataTransfer.getData('data');
+    cargarCancion(data);
     console.log('data' + data)
-    event.target.appendChild(document.getElementById(data));
+    // event.target.appendChild(document.getElementById(data));
    }
+
+  
+function cancion(event){
+  var track_url = 'https://soundcloud.com/tracks/' + event;
+SC.oEmbed(track_url, { auto_play: true }).then(function(oEmbed) {
+  console.log('oEmbed response: ', oEmbed);
+});
+}
+
+function cancion2(event){
+  var track_url = 'https://soundcloud.com/' + event + '\'';
+SC.oEmbed(track_url, { auto_play: true }).then(function(oEmbed) {
+  console.log('oEmbed response: ', oEmbed);
+});
+}
+function cancion3(event){
+  SC.stream('\'' + event + '\'').then(function(player){
+    player.play().then(function(){
+      console.log('Playback started!');
+    }).catch(function(e){
+      console.error('Playback rejected. Try calling play() from a user interaction.', e);
+    });
+  });
+}
+
+function cargarCancion(id){
+  document.getElementById('reproductor').src = "https://w.soundcloud.com/player/?url=https%3A//api.soundcloud.com/tracks/" + id + "&auto_play=true";
+}
+
+
+
+
+
 
 
   
 
 
+// });
+
+
+
+
+// function asignamosCancion(event){
+//   var miAudio =document.getElementById('miAudio').src = event.target.id;
+//   // console.log("La cancion es: " + miAudio);
+//   var duracion=0;
+//   var interval;
+//   miAudio.ondurationchange = function() {
+//     duracion=miAudio.duration;
+    
+//   }
+//  }
+  
+
+
+//  function iniciar(){
+// if(miAudio.paused)
+// {
+//   // iniciamos
+//   miAudio.play();
+//   interval=setInterval(mostrarDuracion,100);
+//   document.getElementById("iniciar").innerHTML="&#8545;";
+// }else{
+//   // paramos
+//   miAudio.pause();
+//   clearInterval(interval);
+//   document.getElementById("iniciar").innerHTML="&#8227;";
+// }
+// }
+
+// function mostrarDuracion(){
+// if(duracion>0)
+// {
+//   porcentaje=miAudio.currentTime*100/duracion;
+//   document.getElementById("duracion").getElementsByTagName("span")[0].style.width=porcentaje+"px";
+//   document.getElementById("duracion").getElementsByTagName("div")[0].innerHTML=parseInt(porcentaje)+"%";
+// }
+// }
+
+// SC.stream('/tracks/293').then(function(player){
+// player.play().then(function(){
+//   console.log('Playback started!');
+// }).catch(function(e){
+//   console.error('Playback rejected. Try calling play() from a user interaction.', e);
+// });
 // });
