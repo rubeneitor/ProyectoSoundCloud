2e183c33be6d91f8932d7c2f41a67b09520cb944
rubeneitor <rvb1492@hotmail.com>
2019-11-10 19:16:20 +0100

    Proyecto acabado con codigo depurado y ultimos detalles

--- js/app.js
+++ js/app.js
@@ -1,134 +1,94 @@
 
 
-// document.addEventListener('DOMContentLoaded', (event) => {
-    // Busqueda();
+//AUTENTIFICAMOS CON EL ID DE CLIENTE
+function autentificar() {
+  SC.initialize({
+    client_id: 'aa06b0630e34d6055f9c6f8beb8e02eb',
+  });
+}
+
+document.getElementById("resultados").innerHTML = "";
+//BUSCAMOS UNA CANCION Y MOSTRAMOS LOS RESULTADOS
+$("#buscar").on('click', function (ev) {
+  
+  $('#resultados').empty(); //Limpiamos la lista.
 
 
-      function Busqueda() {
-    $('.lista').empty(); //Limpiamos la lista.
-    var autor = $('input').val();
-    console.log(autor);
-    
-    SC.initialize({
-      client_id: 'aa06b0630e34d6055f9c6f8beb8e02eb',
-    });
-    let cancion;
-    SC.get('/tracks', {
-    
-      q: document.getElementById('cancion').value,
-    }).then(function(tracks) {
-      var numero = 0;
-      if (tracks.length > 12) {
-        numero = 12;
+  var autor = $('input').val();
+
+  autentificar();
+
+  let cancion;
+  SC.get('/tracks', {
+
+    q: document.getElementById('cancion').value
+    // q: $('#cancion').val(),
+  }).then(function (tracks) {
+    var numero = document.getElementById("numResultados").value;
+    var resultado = document.getElementById('cancion').value;
+
+    if (resultado == '') {
+      alert("Introduce un nombre de cancion, titulo o autor");
+    } else {
+      if ((numero <= 0) || numero == null) {
+        alert('Tienes que insertar un numero de resultados');
       } else {
-        numero = tracks.length;
-      }
-      id = 1;
-      for (var i = 0; i < numero; i++) {
-        if (tracks[i].artwork_url !== null) {
-          $('.lista').append(
-            "<div " + "id =' "+tracks[i].artwork_url +" ' class='imagen_mini col-xs-2'><br><img src='" + 
-              tracks[i].artwork_url + 
-              "' id ='" + 
-              tracks[i].id + console.log(tracks[i].id) + 
-              `' draggable='true' ondragstart='drag(event, ${tracks[i].id})'><p class="titulo">Titulo: ` + tracks[i].title + `<p class="genero">Genero: ` + tracks[i].genre +`</p><p class="autor">Autor: ` + tracks[i].user.username + `</p></div>`
-          );
+        $('.contenedorReproductorYArrastrar').css("visibility", "visible"); 
+        id = 1;
+        //BUCLE DONDE AÑADE UN DIV CON SU IMAGEN Y LOS DATOS QUE INDICO POR CADA RESULTADO DE LA BUSQUEDA
+        for (var i = 1; i <= numero; i++) {
+          if (tracks[i].artwork_url !== null) {
+            console.log("IMAGEN: " + tracks[i].artwork_url);
+            $('.lista').append(
+              "<div " + "id =' " + tracks[i].artwork_url + " '><br><div class='resultado'><img src='" +
+              tracks[i].artwork_url +
+              "' id ='" +
+              tracks[i].id + console.log(tracks[i].id) +
+              `' draggable='true' ondragstart='drag(event, ${tracks[i].id})'><p class="titulo">Titulo: ` + tracks[i].title + `<p class="publicacion">Publicación: ` + tracks[i].release_day + "-" + tracks[i].release_month + "-" + tracks[i].realease_year + `</p><p class="autor">Autor: ` + tracks[i].user.username + `</p></div></div>`
+            );
+          } else {
+            //EN CASO DE QUE LA CANCION NO TENGA IMAGEN, PONGO UNA POR DEFECTO
+            tracks[i].artwork_url = "images/iconoSC.png";
+            $('.lista').append("<div " + "id =' " + tracks[i].artwork_url + " '><br><div class='resultado'><img src='" +
+              tracks[i].artwork_url +
+              "' id ='" +
+              tracks[i].id + console.log(tracks[i].id) +
+              `' draggable='true' ondragstart='drag(event, ${tracks[i].id})'><p class="titulo">Titulo: ` + tracks[i].title + `<p class="publicacion">Publicación: ` + tracks[i].release_day + "-" + tracks[i].release_month + "-" + tracks[i].realease_year + `</p><p class="autor">Autor: ` + tracks[i].user.username + `</p></div></div>`);
+            
+          }
         }
       }
-    })
+    }
+  })
+
     .catch(error => {
       console.error('ups', error);
     });
-    
-   }
 
-  function permitirDrop(event){
-    event.preventDefault();
-   }
-
-   function drag(event, objetoDragado){
-     event.dataTransfer.setData('data', objetoDragado);
-     console.log(objetoDragado);
-   }
-  
-   function drop(event){
-     document.getElementById("reproductor").style="display:block";
-     document.getElementById("cancionArrastrada").innerHTML = "Arrastra otra cancion aqui"
-     event.preventDefault();
-    var data = event.dataTransfer.getData('data');
-    cargarCancion(data);
-    console.log('data' + data)
-    // event.target.appendChild(document.getElementById(data));
-   }
-
-  
-// function cancion(event){
-//   var track_url = 'https://soundcloud.com/tracks/' + event;
-// SC.oEmbed(track_url, { auto_play: true }).then(function(oEmbed) {
-//   console.log('oEmbed response: ', oEmbed);
-// });
-// }
+});
 
 
-function cargarCancion(id){
-  document.getElementById('reproductor').src = "https://w.soundcloud.com/player/?url=https%3A//api.soundcloud.com/tracks/" + id + "&auto_play=true&liking=false&bbuying=false&sharing=false";
+function permitirDrop(event) {
+  event.preventDefault();
 }
 
+// COGEMOS EL ELEMENTO ARRASTRANDO CON EL RATON
+function drag(event, objetoDragado) {
+  event.dataTransfer.setData('data', objetoDragado);
+}
 
+//SOLTAMOS EL ELMENTO PARA MOSTRAR EL REPRODUCTOR QUE INICIALMENTE DEJAMOS EN DISPLAY:NONE
+function drop(event) {
+  document.getElementById("reproductor").style = "display:block";
+  $("#cancionArrastrada").html("Arrastra otra cancion aqui");
+  event.preventDefault();
+  var data = event.dataTransfer.getData('data');
+  cargarCancion(data);
+}
 
+//AQUI CARGAMOS LA CANCION DROPADA EN EL REPRODUCTOR 
+function cargarCancion(id) {
+  document.getElementById('reproductor').src = "https://w.soundcloud.com/player/?url=https%3A//api.soundcloud.com/tracks/" + id + "&auto_play=true&liking=false&bbuying=false&sharing=false";
 
+}
 
-
-
-  
-
-
-// });
-
-
-
-
-// function asignamosCancion(event){
-//   var miAudio =document.getElementById('miAudio').src = event.target.id;
-//   // console.log("La cancion es: " + miAudio);
-//   var duracion=0;
-//   var interval;
-//   miAudio.ondurationchange = function() {
-//     duracion=miAudio.duration;
-    
-//   }
-//  }
-  
-
-
-//  function iniciar(){
-// if(miAudio.paused)
-// {
-//   // iniciamos
-//   miAudio.play();
-//   interval=setInterval(mostrarDuracion,100);
-//   document.getElementById("iniciar").innerHTML="&#8545;";
-// }else{
-//   // paramos
-//   miAudio.pause();
-//   clearInterval(interval);
-//   document.getElementById("iniciar").innerHTML="&#8227;";
-// }
-// }
-
-// function mostrarDuracion(){
-// if(duracion>0)
-// {
-//   porcentaje=miAudio.currentTime*100/duracion;
-//   document.getElementById("duracion").getElementsByTagName("span")[0].style.width=porcentaje+"px";
-//   document.getElementById("duracion").getElementsByTagName("div")[0].innerHTML=parseInt(porcentaje)+"%";
-// }
-// }
-
-// SC.stream('/tracks/293').then(function(player){
-// player.play().then(function(){
-//   console.log('Playback started!');
-// }).catch(function(e){
-//   console.error('Playback rejected. Try calling play() from a user interaction.', e);
-// });
-// });
